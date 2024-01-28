d34500ecd1f77cf8ab6be6ccea0006912d53b5f2
Lebohang1821 <millboylebohang@gmail.com>
2024-01-28 02:38:33 -0800

    task-8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""It starts Flask web application:
+Web application must be listening on 0.0.0.0, port 5000
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """It removes current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
