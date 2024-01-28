5589716de3f77428f0ff6a8bf62a14bf1ebe8e04
Lebohang1821 <millboylebohang@gmail.com>
2024-01-28 02:26:40 -0800

    task-6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""It starts a Flask web application
-Your web application must be listening on 0.0.0.0, port 5000
-    Routes:
-        /: display “Hello HBNB!”
-        /hbnb: display “HBNB”
-        /c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-        /python/(<text>): display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-            The default value of text is “is cool”
-        /number/<n>: display “n is a number” only if n is an integer
-        /number_template/<n>: display a HTML page only if n is an integer:
-            H1 tag: “Number: n” inside the tag BODY
-    You must use the option strict_slashes=False in your route definition
+"""Starts Flask web app
+Routes:
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
+    /python/<text> - display "Python is cool"
+    /number/<n> - display n if integer
+    /number_template/<n> - display a HTML page if n is int
+    /number_odd_or_even/<n> - displays if a number is odd or even
 """
 from flask import Flask, render_template
 
@@ -19,19 +16,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hbnb_route():
-    """It prints Hello HBNB"""
+    """prints Hello HBNB"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """It prints HBNB"""
+    """prints HBNB"""
     return "HBNB"
 
 
 @app.route('/c/<string:text>', strict_slashes=False)
 def c_text(text):
-    """It prints C followed by <text> content"""
+    """prints C followed by <text> content"""
     text = text.replace("_", " ")
     return "C %s" % text
 
@@ -39,26 +36,26 @@ def c_text(text):
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<string:text>', strict_slashes=False)
 def python_text(text="is cool"):
-    """It prints Python is cool"""
+    """prints Python is cool"""
     text = text.replace("_", " ")
     return "Python %s" % text
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_n(n):
-    """It displays n if integer"""
+    """displays n if integer"""
     return "%i is a number" % n
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """It display HTML page if n is int"""
+    """display a HTML page if n is int"""
     return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    """It display HTML page if n is int"""
+    """display a HTML page if n is int"""
     return render_template('6-number_odd_or_even.html', n=n)
 
 
